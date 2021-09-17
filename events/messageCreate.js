66bd3a2c6dc3102cfcf559fdd4de9771765ed273
Nowack.sh <73441963+Mxnyl@users.noreply.github.com>
2021-09-17 14:53:57 +0100

    Add files via upload

---
+++ events/messageCreate.js
@@ -0,0 +1,14 @@
+const prefix = "a!" //Create a variable who stock the prefix.
+const chalk = require("chalk") //A module to add colors in the console
+
+module.exports = (client, message) => {
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+
+    const command = args.shift().toLowerCase();
+
+    if (client.commands.get(command)) {
+        client.commands.get(command).execute(client, message, args);
+        console.log(chalk.bgBlack.white(`The user ${chalk.bgBlack.green(message.author.tag)} as executed ${chalk.bgBlack.green(client.commands.get(command).name)}`));
+    }
+}
