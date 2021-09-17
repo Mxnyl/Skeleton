ab8049697d01f338d8ae79797323746c5649679c
Nowack.sh <73441963+Mxnyl@users.noreply.github.com>
2021-09-17 15:00:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const client = new Discord.Client({
 
 client.commands = new Discord.Collection();
 require('dotenv').config()
-
+require("./utils/loadEvents")(client);
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
